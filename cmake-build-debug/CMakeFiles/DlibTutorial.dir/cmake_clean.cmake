2a0990486736438d01a63264f722011b28bb31be
congshi <congshi.hello@gmail.com>
2021-07-22 11:40:01 +0800

    如何训练飞机检测

---
+++ cmake-build-debug/CMakeFiles/DlibTutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DlibTutorial.dir/main.cpp.o"
+  "DlibTutorial"
+  "DlibTutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DlibTutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
